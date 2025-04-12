21ee2fe0c24c27cfcbacb5ce3e060576a0be3812
Husayn01 <hussainiahmed222@gmail.com>
2025-04-12 20:16:54 +0100

    Solved 1 problem

---
+++ Longest_common_prefix.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        prefix = ""
+        for i in range(len(strs[0])):
+            char = strs[0][i]
+            for word in strs[1:]:
+                if i >= len(word) or word[i] != char:
+                    return prefix
+            prefix += char
+        return prefix
+
+s = Solution()
+print(s.longestCommonPrefix(["flower", "flow", "flight"]))
